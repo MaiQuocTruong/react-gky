b1e1f350acd6aa25a9e323d3545d004aa64fcebd
H82M19\Student <Student>
2024-10-27 14:23:45 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Screen_01 from './screens/Screen_01';
+import Screen_02 from './screens/Screen_02';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //data không sử dụng mockapi
+    //<Screen_01/>
+
+    //data mockapi
+    <Screen_01/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
